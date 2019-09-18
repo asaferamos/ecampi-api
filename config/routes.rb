1ff73cde7ae47330ed47112a4326a23ebfa1b920
Asafe Ramos <asafe@asaferamos.com>
2019-09-17 22:08:53 -0300

    improvements rubocop routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api, path: '/' do
     namespace :v1 do
-      devise_for :users, :controllers => {
-        :registrations => 'devise/registrations',
-        :sessions => 'devise/sessions',
-        :passwords => 'devise/passwords'
+      devise_for :users, controllers: {
+        registrations: 'devise/registrations',
+        sessions:      'devise/sessions',
+        passwords:     'devise/passwords'
       }
+
       resources :accounts
     end
   end
